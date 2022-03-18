6e0b517dd9131db2ecb33c5765344783c2439024
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2022-03-18 17:17:52 +0100

    added constants + reducers from redux for Cart and Product

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,14 @@
-import { createStore } from 'redux';
-import  reducer from './reducer';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+// import  reducer from './reducer';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-const store = createStore(reducer);
+const reducer = combineReducers({ })
+
+// Array with all middleware
+const middleware = [ thunk ]
+
+const store = createStore(reducer, compose(applyMiddleware( thunk )));
 
 export default store;
