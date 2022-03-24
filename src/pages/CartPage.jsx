6df181c7e5f32ebac6f27ac9569639511eb7c68f
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2022-03-24 01:01:56 +0100

    pay w/ paypal + credit card

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,11 +3,80 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+/* STYLES */
 import CartItem from '../components/CartItem/CartItem'
 
+/* ACTIONS */
 import { addToCart, removeFromCart } from '../Redux/actions/cartActions';
 
+/* PAYPAL */
+import {
+    PayPalScriptProvider,
+    PayPalButtons,
+    usePayPalScriptReducer
+} from "@paypal/react-paypal-js";
+
+
 const CartPage = () => {
+    // This values are the props in the UI
+    const amount = "2";
+    const currency = "JPY";
+    const style = { "layout": "vertical" };
+    // Custom component to wrap the PayPalButtons and handle currency changes
+    const ButtonWrapper = ({ currency, showSpinner }) => {
+        // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+        // This is the main reason to wrap the PayPalButtons in a new component
+        const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+        useEffect(() => {
+            dispatch({
+                type: "resetOptions",
+                value: {
+                    ...options,
+                    currency: currency,
+                },
+            });
+        }, [currency, showSpinner]);
+
+
+        return (<>
+            {(showSpinner && isPending) && <div className="spinner" />}
+            <PayPalButtons
+                style={style}
+                disabled={false}
+                forceReRender={[amount, currency, style]}
+                fundingSource={undefined}
+                createOrder={(data, actions) => {
+                    return actions.order
+                        .create({
+                            purchase_units: [
+                                {
+                                    amount: {
+                                        currency_code: currency,
+                                        value: amount,
+                                    },
+                                },
+                            ],
+                        })
+                        .then((orderId) => {
+                            // Your code here after create the order
+                            return orderId;
+                        });
+                }}
+                onApprove={function (data, actions) {
+                    return actions.order.capture().then(function () {
+                        // Your code here after capture the order
+                    });
+                }}
+            />
+        </>
+        );
+    }
+
+
+
+
+
     const dispatch = useDispatch()
     const cart = useSelector(state => state.cart);
     const { cartItems } = cart;
@@ -47,7 +116,21 @@ const CartPage = () => {
                 </tbody>
             </table>
             <p>Item</p>
+
+            <PayPalScriptProvider
+                options={{
+                    "client-id": "test",
+                    components: "buttons",
+                    currency: "JPY"
+                }}
+            >
+                <ButtonWrapper
+                    currency={currency}
+                    showSpinner={false}
+                />
+            </PayPalScriptProvider>
         </>
+
     )
 }
 
